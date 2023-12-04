c4aaf76ba6d6d7877b2327ebe5b7c80b0541b5e4
Agustinus Leonardo Dwitama <agustinus.dwitama@binus.ac.id>
2023-12-05 02:06:42 +0700

    mantap cape

--- script/register_script.js
+++ script/register_script.js
@@ -13,6 +13,9 @@ window.onload = function () {
         'input[name="genders"]:checked'
       ).value;
       let isValid = false;
+      let isValid2 = false;
+      let isValid3 = false;
+      let isValid4 = false;
 
       // Email Validation
       function validateEmail(email) {
@@ -40,28 +43,43 @@ window.onload = function () {
       // end of Email Validation
 
       // Password validation
+      if (password.length < 8) {
+        alert("Password must be 8 characters or above");
+        isValid4 = false;
+      } else {
+        isValid4 = true;
+      }
+      // end of Password validation
+
+      // Password confirmation validation
       if (password !== confirmPassword) {
         alert("Passwords do not match.");
-        isValid = false;
+        isValid2 = false;
         return;
       } else {
-        isValid = true;
+        isValid2 = true;
       }
       // end of Password validation
 
       // Terms and condition validation
       if (!document.getElementById("terms_btn").checked) {
         alert("Terms and condition agreement must be checked");
-        isValid = false;
+        isValid3 = false;
       } else {
-        isValid = true;
+        isValid3 = true;
       }
       // end of Terms and condition validation
 
       //creds validation
-      if (isValid !== false) {
+      if (
+        isValid !== false &&
+        isValid2 !== false &&
+        isValid3 !== false &&
+        isValid4 !== false
+      ) {
         console.log("registration success");
         alert("registration success");
+        window.location.href = "../html/homePage.html";
       }
       //end of creds validation
 
